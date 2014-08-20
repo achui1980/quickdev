c0a9cc4cf5e3847e45fefea7bffcd6f2d1ecb63a
achui <achui_1980@163.com>
2014-08-20 22:50:23 +0800

    update

--- parent/app-web/src/main/java/com/achui/quick/service/UserRoleService.java
+++ parent/app-web/src/main/java/com/achui/quick/service/UserRoleService.java
@@ -1,12 +1,16 @@
 package com.achui.quick.service;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.achui.quick.common.service.BaseService;
+import com.achui.quick.domain.SysPermission;
 import com.achui.quick.domain.SysRole;
+import com.achui.quick.domain.SysUser;
 import com.achui.quick.domain.SysUserRole;
 import com.achui.quick.repository.UserRoleRepository;
 
@@ -23,5 +27,21 @@ public class UserRoleService extends BaseService<SysUserRole, Integer>{
 	public List<SysRole> getRoles(List<Integer> ids){
 		return userRoleRepository.getRolesByIds(ids);
 	}
+	
+	public List<String> getPermissions(SysUser user){
+		/**
+		 * 1.user对应的role
+		 * 2、role对应的resouce
+		 * 3、resouce对应的permission
+		 * 格式：sys:user:create 或者 sys:*:create
+		 */
+		List<SysRole> roleList = this.getRoles(Arrays.asList(user.getId()));
+		for(SysRole role : roleList){
+			String ql = " from SysRoleResoucePermission obj where obj.roleId = :roleId";
+			userRoleRepository.findAll(ql, (Sort)null, null);
+		}
+		return null;
+	}
+	
 
 }
