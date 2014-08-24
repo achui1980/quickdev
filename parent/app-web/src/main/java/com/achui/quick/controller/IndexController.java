26f971ff1aac8d9c706f2bdaadc7b37ae3f59cdf
snowgxn <snowgxn@snowgxn-PC>
2014-08-24 22:38:31 +0800

    update

--- parent/app-web/src/main/java/com/achui/quick/controller/IndexController.java
+++ parent/app-web/src/main/java/com/achui/quick/controller/IndexController.java
@@ -5,10 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.achui.quick.domain.SysUser;
 import com.achui.quick.service.MyUserService;
 
 @Controller
@@ -34,5 +39,15 @@ public class IndexController {
 		SecurityUtils.getSubject().login(new UsernamePasswordToken(userName,password));
 		model.addAttribute("name","index");
 		return "index";
+	}
+	
+	@RequestMapping(value="/user/{id}" , method=RequestMethod.GET,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody SysUser getUser(@PathVariable("id") Integer id){
+		SysUser user = new SysUser();
+		user.setId(id);
+		user.setUsername("achui");
+		user.setPassword("123456");
+		return user;
 	}
 }
