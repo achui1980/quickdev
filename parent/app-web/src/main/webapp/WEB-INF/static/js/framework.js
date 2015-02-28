9fa04857cf693637a3fc5341dd8dbbf14b20292f
portz <portz@L206.ehi.ehealth.com>
2015-02-28 17:17:31 +0800

    Signed-off-by: portz <portz@L206.ehi.ehealth.com>

--- parent/app-web/src/main/webapp/WEB-INF/static/js/framework.js
+++ parent/app-web/src/main/webapp/WEB-INF/static/js/framework.js
@@ -2,8 +2,16 @@ webix.ready(function(){
 	webix.ui({
 		id:'framework',
 		rows:[
-		   {template:"row 1", height:40 },
-		   {template:"row 2"},
+		   {template:"row 1", height:50},
+		   {
+			   cols:[
+			      {template:"col 1", width:150},
+			      {
+                      view:"resizer"
+                  },
+			      {template:"col 2"},
+			   ]
+		   },
 		   {template:"row 3", height:40 },
 		]
 	});
